2b9bc511b77cf8f857dd791571efe9d967452631
kadodds <kadodds@microsoft.com>
2017-08-28 10:41:07 -0700

    Two timer functions capable of sharing in-memory data

--- KeyVaultPersistencePOC/SimpleTimerTest.cs
+++ KeyVaultPersistencePOC/SimpleTimerTest.cs
@@ -1,15 +1,25 @@
 using System;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
+using System.Runtime.Caching;
 
 namespace KeyVaultPersistencePOC
 {
     public static class SimpleTimerTest
     {
-        [FunctionName("TimerTriggerCSharp")]
+        static MemoryCache memoryCache = MemoryCache.Default;
+
+        [FunctionName("TimerTrigger1")]
         public static void Run([TimerTrigger("0 */5 * * * *")]TimerInfo myTimer, TraceWriter log)
         {
-            log.Info($"C# Timer trigger function executed at: {DateTime.Now}");
+            log.Info($"SimpleTimerTest executed at: {DateTime.Now}");
+
+            var cacheObject = memoryCache["cachedCount"];
+            var cachedCount = (cacheObject == null) ? 0 : (int) cacheObject;
+            memoryCache.Set("cachedCount", ++cachedCount, DateTimeOffset.Now.AddMinutes(20));
+
+            log.Info($"TimerTest triggered memory count {cachedCount}");
+
         }
     }
 }
