2a7483152a075de70f0c69bc6ffbf3194bb8089e
kadodds <kadodds@microsoft.com>
2017-08-29 15:58:20 -0700

    Added a service bus writter http function

---
+++ KeyVaultPersistencePOC/ServiceBusWriterHTTP.cs
@@ -0,0 +1,80 @@
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Host;
+using System.Runtime.Caching;
+using Starbucks.Dp.Platform.ServiceBusWriter;
+using Starbucks.Dp.Platform.Shared;
+using Starbucks.Dp.Platform.Shared.CircuitBreaker;
+using System.Collections.Generic;
+
+namespace KeyVaultPersistencePOC
+{
+    public static class ServiceBusWriterHTTP
+    {
+
+        // cache
+        static MemoryCache memoryCache = MemoryCache.Default;
+
+        [FunctionName("ServiceBusWriterHTTP")]
+        public static async Task<HttpResponseMessage> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)]HttpRequestMessage req, TraceWriter log)
+        {
+            log.Info($"ServiceBusWriterTimer function executed at: {DateTime.Now}");
+
+            /** Service Bus Connection Strings **/
+            var serviceBusEndpoint = "Endpoint=sb://s00197svb0platmsgdev0.servicebus.windows.net/";
+            var sendPolicyName = "activetestqueuemanage";
+            var entityPath = "active-test-queue";
+
+            /** Attempt to gather secrets from cache **/
+            log.Info("Reading 'ServiceBusSendPrimaryKey' from cache..");
+            var cacheObject1 = memoryCache["ServiceBusSendPrimaryKey"];
+            var primaryKey = (cacheObject1 == null) ? null : (string)cacheObject1;
+
+            log.Info("Reading 'ServiceBusSendSecondaryKey' from cache..");
+            var cacheObject2 = memoryCache["ServiceBusSendSecondaryKey"];
+            var secondaryKey = (cacheObject2 == null) ? null : (string)cacheObject2;
+
+            /** Handle case that cached objects returned null **/
+            if (primaryKey == null || secondaryKey == null)
+            {
+                /** Do something here to handle cache failure **/
+                log.Warning("Secret key values return null..exiting function");
+                return req.CreateErrorResponse(HttpStatusCode.BadRequest, "Secret values not found.");
+            }
+
+            /** Service Bus Connection Config **/
+            log.Info("Setting up ServiceBusWriterConfig objects..");
+            var primaryWriterConfig = new ServiceBusWriterConfig(new NullEncryptor(), new NullCompressor(),
+                primaryKey)
+            {
+                SendPolicyName = sendPolicyName,
+                ServiceBusEndpoint = serviceBusEndpoint,
+                UseCompression = false,
+                UseEncryption = false,
+                EntityPath = entityPath
+            };
+            var secondaryWriterConfig = new ServiceBusWriterConfig(new NullEncryptor(), new NullCompressor(),
+                secondaryKey, ConnectionType.Secondary)
+            {
+                SendPolicyName = sendPolicyName,
+                ServiceBusEndpoint = serviceBusEndpoint,
+                UseCompression = false,
+                UseEncryption = false,
+                EntityPath = entityPath
+            };
+
+            /** Send message **/
+            log.Info("Sending message to Service Bus Queue..");
+            var sender = new WriteServiceBusQueueCommand(primaryWriterConfig, secondaryWriterConfig);
+            var result = await sender.WriteMessageAsync("This is a simple message to the Service Bus Queue!", new Dictionary<string, object>());
+
+            log.Info("Messages sent");
+
+            return req.CreateResponse(HttpStatusCode.OK, "Message sent");
+        }
+    }
+}
